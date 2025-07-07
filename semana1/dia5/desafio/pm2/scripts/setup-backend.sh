2e1092f0a5c28d294eba398bf9e8497b91cff8a2
Litocruz <julian.lamadrid@gmail.com>
2025-07-07 12:25:55 +0200

    Modularizacion de los scripts

--- semana1/dia5/desafio/pm2/scripts/setup-backend.sh
+++ semana1/dia5/desafio/pm2/scripts/setup-backend.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
-echo "🐍 Setup services"
+LOG="ecommerce.log"
 
-#python3 -m venv /home/vagrant/pokemon-env
-#source /home/vagrant/pokemon-env/bin/activate
+deploy-services() {  
+  echo "🐍 Setup services" | tee -a $LOG
+  
+  declare -A services
+  sevices[frontend]="3000"
+  sevices[products]="3001"
+  sevices[shopping-cart]="3002"
+  sevices[merchandise]="3003"
+  
+  for service in "${!services[@]}"; do
+      echo "$service: ${services[$service]}" | tee -a $LOG
+      cd /vagrant/$service >> $LOG 2>&1
+      npm install >> $LOG 2>&1
+      pm2 start server.js --name $service -- -p ${services[$service]} >> $LOG 2>&1
+  done
+  echo "Aplicaciones configuradas y lanzadas" | tee -a $LOG
+}
 
-declare -A services
-sevices[frontend]="3000"
-sevices[products]="3001"
-sevices[shopping-cart]="3002"
-sevices[merchandise]="3003"
+save-services(){
+  echo "Guardando servicios pm2" | tee -a $LOG
+  pm2 save >> $LOG 2>&1
+  pm2 startup systemd -u $USER --hp $HOME >> ../$LOG 2>&1
+}
 
-for service in "${!services[@]}"; do
-    echo "$service: ${services[$service]}"
-    cd /vagrant/$service
-    npm install
-    pm2 start server.js --name $service -- -p ${services[$service]}
-done
-echo "Estado Aplicaciones"
-pm2 list
-pm2 save
-#cd /vagrant/backend
-#pip install -r requirements.txt
+service-check(){
+  echo "Estado de los servicios "| tee -a $LOG
+  pm2 save >> $LOG 2>&1
+}
 
-echo "✅ Backends configurado"
+deploy-services
+save-services
+service-check
+
+echo "✅ Backends configurado" | tee -a $LOG
--- semana1/dia5/desafio/pm2/scripts/setup-system.sh
+++ semana1/dia5/desafio/pm2/scripts/setup-system.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-echo "🔴 Ecommerce - Setup Sistema"
-echo "90 Days of DevOps by Roxs"
+LOG="ecommerce.log"
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get update -y
+install-dependencies(){
+  echo "🔴 Ecommerce - Setup Sistema" | tee -a $LOG
+  echo "90 Days of DevOps by Roxs" | tee -a $LOG
+  apt-get update -y >> $LOG 2>&1
+  apt-get install -y curl wget git vim htop python3 python3-pip python3-venv >> $LOG 2>&1
+  curl -fsSL https://deb.nodesource.com/setup_20.x | bash - >> $LOG 2>&1
+  apt-get install -y nodejs >> $LOG 2>&1
+  npm install -g npm@11.4.2 >> $LOG 2>&1
+  sudo npm install -g pm2 >> $LOG 2>&1
 
-apt-get install -y curl wget git vim htop python3 python3-pip python3-venv
+  echo "✅ Sistema configurado" | tee -a $LOG
+}
 
-curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
-apt-get install -y nodejs
+install-dependencies
 
-npm install -g npm@11.4.2
-
-sudo npm install -g pm2
-
-echo "✅ Sistema configurado"
